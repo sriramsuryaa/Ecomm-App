1df9d88bee5fda1ea435f8f0161440dc05c01cb7
sriramsuryaa <sriramsuryaa@outlook.com>
2026-04-08 14:16:36 +0530

    cleared the unwanted lines

--- build.sh
+++ build.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# docker build -t ${env.REPO}:${env.IMAGE_TAG} .
-# echo \$DHUB_PASS | docker login -u \$DHUB_USER --password-stdin
-# docker push ${env.REPO}:${env.IMAGE_TAG}
-
 docker build -t $REPO:$IMAGE_TAG .
 docker tag $REPO:$IMAGE_TAG $REPO:$IMAGE_TAG
 docker push $REPO:$IMAGE_TAG
--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
-#!/usr/bin/env bash
-set -euo pipefail
+#!/bin/bash
+set -e
 
 cd /home/ubuntu/deploy/
 
