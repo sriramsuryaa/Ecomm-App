72a4cfac1c1acc71707449220736572eb6be37da
sriramsuryaa <sriramsuryaa@outlook.com>
2026-04-08 11:57:33 +0530

    updated the build file

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-docker build -t ${env.REPO}:${env.IMAGE_TAG} .
+# docker build -t ${env.REPO}:${env.IMAGE_TAG} .
+# echo \$DHUB_PASS | docker login -u \$DHUB_USER --password-stdin
+# docker push ${env.REPO}:${env.IMAGE_TAG}
+
+docker build -t $REPO:$IMAGE_TAG .
 echo \$DHUB_PASS | docker login -u \$DHUB_USER --password-stdin
-docker push ${env.REPO}:${env.IMAGE_TAG}
+docker push $REPO:$IMAGE_TAG
